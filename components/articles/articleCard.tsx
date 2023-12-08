540cb0559b03e512e40d865d4df1d7ee160e0580
clement <clement.sauvonnet@gmail.com>
2023-12-08 03:02:26 +0100

    comit page articles

--- components/articles/articleCard.tsx
+++ components/articles/articleCard.tsx
@@ -1,20 +1,28 @@
 import Image from "next/image";
+import '@/styles/global.css';
+import Link from "next/dist/client/link";
 
-export default function CardArticle(props : any){
-    return (<div><div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <Image className="w-full" src="/assets/images/auth-dark.png" alt="Sunset in the mountains" width ={150} height={150}/>
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">{props.title}</div>
-      <p className="text-gray-700 text-base">
-        {props.description}
-        </p>
+export default function CardArticle(props: any) {
+  return (
+    <Link href={props.link}>
+     
+    <div className="w-[250px] mb-10 overflow-y-auto max-h-[400px]">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          className="w-full h-56 object-cover"
+          src={props.imageName}
+          alt="Image"
+          width={150}
+          height={150}
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{props.title}</div>
+          <p className="text-gray-700 text-base">{props.description}</p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+        </div>
+      </div>
     </div>
-    <div className="px-6 pt-4 pb-2">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-    </div>
-  </div>
-  </div>
+    </Link>
   );
 }
