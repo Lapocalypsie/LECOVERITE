cd84aeb7631ef17de81b3506946517a69ee3793d
Lapocalypsie <jrasolosoa@gmail.com>
2023-12-08 04:14:02 +0100

    Update game page UI

--- app/(root)/games/page.tsx
+++ app/(root)/games/page.tsx
@@ -1,10 +1,11 @@
-import Jeu from "@/components/game/jeu";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
-      <Jeu />
+      <h1 className="my-10 text-center text-4xl font-bold">
+        Selectionnez un jeu au auquel vous voulez jouer
+      </h1>
     </div>
   );
 };
